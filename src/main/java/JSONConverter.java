7a7c3a8fd0aa6fce863748f3611e6e4616fe2162
Peter <dchen@albany.edu>
2017-09-07 23:45:27 -0400

    Added some comments of the two java program

--- src/main/java/JSONConverter.java
+++ src/main/java/JSONConverter.java
@@ -9,25 +9,28 @@ public class JSONConverter {
     public static void main(String[] args) throws IOException, JSONException {
 
         JSONArray jsonArray = new JSONArray();
-
+        /* Read temp result into the buffer */
         BufferedReader br = new BufferedReader(new FileReader("/Users/Peter/src/SentimentAnalysis/src/main/resources/output/part-r-00000"));
-        String line = br.readLine();
+        String line = br.readLine(); /* Read per line */
+        /* Convert the result to json inorder to use js to show the result */
         FileWriter fileWriter = new FileWriter("/Users/Peter/src/SentimentAnalysis/sentiment-visualization/data/result.json");
-
+        /* Important steps */
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
         while (line != null) {
             JSONObject article = new JSONObject();
+            /* fileName + sentiment + count */
             String[] title_emotion_count = line.split("\t");
             JSONObject emotionList = new JSONObject();
-
+            /* sentiment + count */
             emotionList.put(title_emotion_count[1], title_emotion_count[2]);
             article.put("title", title_emotion_count[0]);
-            for (int i = 0; i < 2; i++) {
+            for (int i = 0; i < 2; i++) { /* why for this loop */
                 line = br.readLine();
                 title_emotion_count = line.split("\t");
                 emotionList.put(title_emotion_count[1], title_emotion_count[2]);
             }
+
             article.put("data", emotionList);
             jsonArray.put(article);
 
--- src/main/java/SentimentAnalysis.java
+++ src/main/java/SentimentAnalysis.java
@@ -18,19 +18,23 @@ import java.util.Map;
 public class SentimentAnalysis {
 
     public static class SentimentSplit extends Mapper<Object, Text, Text, IntWritable> {
-
+        /* Simple cache for comparison later */
         public Map<String, String> emotionDic = new HashMap<String, String>();
 
         @Override
         public void setup(Context context) throws IOException{
             Configuration configuration = context.getConfiguration();
+            /* Get from the set */
             String dicName = configuration.get("dictionary", "");
-
+            /* Load the dictionary */
             BufferedReader br = new BufferedReader(new FileReader(dicName));
+            /* Read per line due to the txt data structure */
             String line = br.readLine();
 
             while (line != null) {
+                /* Break the words by tab */
                 String[] word_feeling = line.split("\t");
+                /* Key-Value : word - word_sentiment */
                 emotionDic.put(word_feeling[0].toLowerCase(), word_feeling[1]);
                 line = br.readLine();
             }
@@ -38,13 +42,18 @@ public class SentimentAnalysis {
         }
 
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context) throws
+                IOException, InterruptedException {
 
+            /* Get the file name */
             String fileName = ((FileSplit) context.getInputSplit()).getPath().getName();
             String line = value.toString().trim();
-            String[] words = line.split("\\s+");
+            String[] words = line.split("\\s+"); /* By one or more spaces */
             for (String word: words) {
+                /* Only write those in the Map */
+                /* Primary key must add the file name */
                 if (emotionDic.containsKey(word.trim().toLowerCase())) {
+                    /* Just need the total number of Positive, neutral and negative words */
                     context.write(new Text(fileName + "\t" + emotionDic.get(word.toLowerCase())), new IntWritable(1));
                 }
             }
@@ -71,6 +80,7 @@ public class SentimentAnalysis {
     public static void main(String[] args) throws Exception {
 
         Configuration configuration = new Configuration();
+        /* Set the dic and then load the dic to the memo */
         configuration.set("dictionary", args[2]);
 
         Job job = Job.getInstance(configuration);
